5998470239d639ac8a6e2ae1ffab3c36972d2391
iniw <andrade.vinicius934@gmail.com>
2021-12-02 00:21:12 -0300

    day one

---
+++ 1/src/main.rs
@@ -0,0 +1,39 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+
+fn main() {
+	let file = File::open("input.txt").expect("something went wrong!");
+
+	let input: Vec<u32> = io::BufReader::new(file)
+		.lines()
+		.map(|x| x.unwrap().parse().unwrap())
+		.collect();
+
+	// part one
+	let mut iter = input.iter().rev().peekable();
+	let mut counter = 0;
+
+	while let Some(x) = iter.next() {
+		if let Some(next) = iter.peek() {
+			if x > *next {
+				counter += 1;
+			}
+		}
+	}
+
+	println!("part one: {:?}", counter);
+
+	// part two
+	let mut iter = input.windows(3).peekable();
+	let mut counter = 0;
+
+	while let Some(x) = iter.next() {
+		if let Some(next) = iter.peek() {
+			if x.iter().sum::<u32>() < next.iter().sum() {
+				counter += 1;
+			}
+		}
+	}
+
+	println!("part two: {:?}", counter)
+}
